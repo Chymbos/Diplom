ba723f7f52b3e23bf4ce66e071f4c9b9e52828cb
Chymbos <127796096+Chymbos@users.noreply.github.com>
2024-05-22 10:05:12 +0200

    Add files via upload

---
+++ api/authentication.py
@@ -0,0 +1,14 @@
+from rest_framework import authentication
+from main.models import Token
+
+
+class TokenAuthentication(authentication.BaseAuthentication):
+
+    def authenticate(self, request):
+        token = request.META.get('HTTP_AUTHORIZATION')
+        print(token)
+        try:
+            return Token.objects.get(token=token).user, None
+        except Token.DoesNotExist:
+            pass
+        return None
